638e0ec9b0d85414cbcafd194a9631c9d3689095
targax <brunotarga08@gmail.com>
2025-07-28 23:23:36 -0300

    codigo final

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -20,8 +22,26 @@ public class Main {
         mentoria.setDescricao("descricao mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devBruno = new Dev();
+        devBruno.setNome("Bruno");
+        devBruno.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos Bruno:" + devBruno.getConteudosInscritos());
+        devBruno.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos Inscritos Bruno:" + devBruno.getConteudosInscritos());
+        System.out.println("Conteudos Concluidos Bruno:" + devBruno.getConteudosConcluidos());
+        System.out.println("XP:" + devBruno.calcularTotalXp());
+
     }
 }
